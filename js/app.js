9e0dff4756724a500b005bc8d8cadd90347450dc
LEONARDO SCHULER <leonardo_schuler55@hotmail.com>
2024-06-15 21:25:19 -0300

    alerta de quantidade valida app.js

--- js/app.js
+++ js/app.js
@@ -7,23 +7,29 @@ function adicionar() {
     //recuperar valores nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
     let quantidade = document.getElementById('quantidade').value;
-    let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1];
 
-    //calcular o preço, o nosso subtotal
-    let preco = quantidade * valorUnitario;
+    //alerta se o usuário não inserir um produto ou uma quantidade válida
+    if (quantidade >=1) {
+        let nomeProduto = produto.split('-')[0];
+        let valorUnitario = produto.split('R$')[1];
 
-    //adicionar o produto no carrinho
-    let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-        </section>`;
+        //calcular o preço, o nosso subtotal
+        let preco = quantidade * valorUnitario;
 
-    //atualizar o valor total da compra 
-    totalGeral = totalGeral + preco;
-    let campoTotal = document.getElementById('valor-total');
-    campoTotal.textContent = `R$ ${totalGeral}`;
-    document.getElementById('quantidade').value = '';
+        //adicionar o produto no carrinho
+        let carrinho = document.getElementById('lista-produtos');
+        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+            </section>`;
+
+        //atualizar o valor total da compra 
+        totalGeral = totalGeral + preco;
+        let campoTotal = document.getElementById('valor-total');
+        campoTotal.textContent = `R$ ${totalGeral}`;
+        document.getElementById('quantidade').value = '';
+    } else {
+        alert ('Adicione uma quantidade válida!');
+    }
 }
 
 function limpar(){
